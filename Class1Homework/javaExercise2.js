6bc71381540fe645f7b1625d1836147885f4532b
BoPetkov <45241400+BoPetkov@users.noreply.github.com>
2018-11-23 16:03:38 +0100

    Delete javaExercise2.js

--- Class1Homework/javaExercise2.js
+++
@@ -1,8 +0,0 @@
-function Price(phones,price){
-        var rate = ((phones * price) * 5) / 100;
-        var tax = ((phones * price) * 18) / 100;        
-        
-        var result = phones * price - rate - tax;
-
-        return(result);
-    }
