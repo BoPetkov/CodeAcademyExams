249ff308f07987c0cbda26b5385d24193e3fedfd
BoPetkov <45241400+BoPetkov@users.noreply.github.com>
2018-11-23 16:02:06 +0100

    Add files via upload

---
+++ Class3Homework/class3Excercise1.js
@@ -0,0 +1,7 @@
+function concata(string1, string2, boolean){
+  if(boolean == true){
+    console.log(string1+string2);
+  }else if(boolean == false){
+    console.log(string2+string1);
+  }else {console.log("nooooooo");}
+}
