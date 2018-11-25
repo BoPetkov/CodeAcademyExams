02b6f8cd73d0d3e7bbc7dce82189af244304c0c1
BoPetkov <45241400+BoPetkov@users.noreply.github.com>
2018-11-25 15:52:44 +0100

    Delete class3Excercise1.js

--- Class3Homework/class3Excercise1.js
+++
@@ -1,7 +0,0 @@
-function concata(string1, string2, boolean){
-  if(boolean == true){
-    console.log(string1+string2);
-  }else if(boolean == false){
-    console.log(string2+string1);
-  }else {console.log("nooooooo");}
-}
