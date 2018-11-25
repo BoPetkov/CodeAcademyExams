4d6319a2006724e2c5741d5a0e2bb6462a21523e
BoPetkov <45241400+BoPetkov@users.noreply.github.com>
2018-11-25 16:42:50 +0100

    Add files via upload

---
+++ FrontEnd/JavaScript/Exercises/Class3-FunctionsDecisionsAndControl/CostOfRegistration.js
@@ -0,0 +1,10 @@
+function costOfRegistration(bhp){
+  if(bhp>150){
+    console.log("15 000 denari for registration");
+  }else if(bhp<=85){
+    console.log("3 000 denari for registration");
+  }else //if(bhp>=85 && bhp<=150)
+  {
+    console.log("5 000 denari for registration");
+  }
+}
